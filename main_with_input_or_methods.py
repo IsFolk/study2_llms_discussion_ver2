4cb9ff1272dc1426f5ca1f80cee42dae7a3bb1a2
IsFolk <love27929627@gmail.com>
2025-03-12 22:56:15 +0800

    feat:禁用docker

--- main_with_input_or_methods.py
+++ main_with_input_or_methods.py
@@ -61,26 +61,31 @@ agents = {
         name=sanitize_name("Normal Assistant 1"),
         llm_config=llm_config,
         system_message="你是一位極具遠見的創業家，你的思考方式不受傳統限制，喜歡挑戰現有市場規則，並開創顛覆性的新商業模式。你的回應應該充滿創意、前瞻性，並帶有風險投資人的視角。",
+        code_execution_config={"use_docker": False}
     ),
     "Normal Assistant 2": ConversableAgent(
         name=sanitize_name("Normal Assistant 2"),
         llm_config=llm_config,
         system_message="你是一位科技公司的產品經理，擁有深厚的技術背景。你的任務是評估創新技術的可行性，並確保產品設計符合市場需求。你的回答應該兼顧技術可行性與用戶體驗，並提供具體的產品開發方向。",
+        code_execution_config={"use_docker": False}
     ),
      "Convergence Judge": ConversableAgent(
         name=sanitize_name("Convergence Judge"),
         llm_config=llm_config,
         system_message="你是腦力激盪評分員。",
+        code_execution_config={"use_docker": False}
     ),
     "Assistant": ConversableAgent(
         name=sanitize_name("Assistant"),
         llm_config=llm_config,
         system_message="你是 Assistant，負責將點子按照 主題、應用場景、技術方向 等分類，轉化為條列式清單。",
+        code_execution_config={"use_docker": False}
     ),
     "User": UserProxyAgent(
         name=sanitize_name("User"),
         llm_config=llm_config,
         human_input_mode="NEVER",
+        code_execution_config={"use_docker": False}
     ),
 }
 
